e030e4b5529dfd5d864d52ea7ca4bbe9f854f04a
Laurens Art Ramsenthaler <artoflaurens@gmail.com>
2022-11-03 16:13:37 +0100

    first

--- passing.py
+++ passing.py
@@ -10,6 +10,7 @@ game_file_name = "game_file.json"
 
 app = Flask(__name__)
 
+
 def readGameState():
     text = ''
     with open(game_file_name, 'r') as f:
@@ -23,15 +24,16 @@ def readGameState():
 
     return game_state
 
+
 def saveGameState(game_state):
     text = json.dumps(game_state)
     with open(game_file_name, 'w') as f:
         f.write(text)
         f.close()
 
+
 @app.route('/')
 def index():
-
     return render_template('test.html')
 
 
@@ -42,10 +44,8 @@ def test():
     output = request.get_json()
     result = json.loads(output)
 
-
     if "player_one" in game_state:
         if "player_two" in game_state:
-
             f = open('game_file.json', 'w')
             f.write("")
             f.close()
@@ -56,13 +56,16 @@ def test():
             print("error")
 
         else:
-            game_state["player_two"] = {"name": result, "points": 100, "wins": 0}
+            game_state["player_two"] = {"name": result, "points": 100, "wins": 0, "current_input": 0,
+                                        "EnteredNumber": False, "GameIsOn": True, "HasWon": False}
     else:
-        game_state["player_one"] = {"name": result, "points": 100, "wins": 0}
+        game_state["player_one"] = {"name": result, "points": 100, "wins": 0, "current_input": 0,
+                                    "EnteredNumber": False, "GameIsOn": True, "HasWon": False}
 
     saveGameState(game_state)
 
     return game_state
+
 
 @app.route('/numbers', methods=['POST'])
 def test_numbers():
@@ -74,102 +77,89 @@ def test_numbers():
     # TODO: get from JS!
     player_name = output_numbers["name"]
     player_number = output_numbers["number"]
-    print(output_numbers )
+    player_current_input = output_numbers["number"]
+    print(output_numbers)
     print(game_state)
 
-    updateNumbers(game_state, player_name, player_number)
+    updateNumbers(game_state, player_name, player_number, player_current_input)
     saveGameState(game_state)
 
     return game_state
+
 
 @app.route('/GameStatus', methods=['GET'])
 def getStatus_js():
     game_state = readGameState()
     return game_state
 
-def updateNumbers(game_state, player_name, res_num):
+
+def updateNumbers(game_state, player_name, res_num, player_current_input):
     # game_state.get()
 
+    # check if both have entered an change to false
+    if game_state["player_one"]["EnteredNumber"] == True and game_state["player_two"]["EnteredNumber"] == True:
+        game_state["player_one"]["EnteredNumber"] = False
+        game_state["player_two"]["EnteredNumber"] = False
+
+
     if game_state["player_one"]["name"] == player_name:
-        game_state["player_one"]["points"] -= int(res_num)
+        points_p1 = game_state["player_one"]["points"]
+        # if player has 4/5 ad enouh points to win rithzrt way then its already won
+
+        if points_p1 == 0:
+            print("Game Over P1")
+            return
+        if not game_state["player_one"]["EnteredNumber"]:
+            if points_p1 - int(player_current_input) > 0:
+                game_state["player_one"]["points"] -= int(res_num)
+                game_state["player_one"]["current_input"] = player_current_input
+                game_state["player_one"]["EnteredNumber"] = True
+                checkWinner(game_state)
+            else:
+                game_state["player_one"]["current_input"] = game_state["player_one"]["points"]
+                game_state["player_one"]["points"] = 0
+                game_state["player_one"]["EnteredNumber"] = True
+                game_state["player_one"]["GameIsOn"] = False
+                checkWinner(game_state)
+
     elif game_state["player_two"]["name"] == player_name:
-        game_state["player_two"]["points"] -= int(res_num)
+        if game_state["player_two"]["points"] == 0:
+            print("Game Over P1")
+            return
+        if not game_state["player_two"]["EnteredNumber"]:
+            if game_state["player_two"]["points"] - int(player_current_input) > 0:
+                game_state["player_two"]["points"] -= int(res_num)
+                game_state["player_two"]["current_input"] = player_current_input
+                game_state["player_two"]["EnteredNumber"] = True
+                checkWinner(game_state)
+            else:
+                game_state["player_two"]["current_input"] = game_state["player_two"]["points"]
+                game_state["player_two"]["points"] = 0
+                game_state["player_two"]["EnteredNumber"] = True
+                game_state["player_two"]["GameIsOn"] = False
+                checkWinner(game_state)
     else:
-        print("Error!!!")
+        print("error")
 
-def doLogic(res):
-    min_score_to_win = 5
+def checkWinner(game_state):
 
-    class MyValues():
-        Points = 100
-        Wins = 0
-        HasWon = False
-        Name = ""
+    if game_state["player_one"]["EnteredNumber"] == True and game_state["player_two"]["EnteredNumber"] == True:
+        current_input_p1 = game_state["player_one"]["current_input"]
+        current_input_p2 = game_state["player_two"]["current_input"]
+        if int(current_input_p1) > int(current_input_p2):
+            print("wins p1 +1")
+            game_state["player_one"]["wins"] += 1
 
-    GamerOne = MyValues()
-    GamerTwo = MyValues()
+        elif int(current_input_p2) > int(current_input_p1):
+            print("wins p2 +1")
+            game_state["player_two"]["wins"] += 1
 
-    GamerOne.Name = res["firstplayer"]
-    GamerTwo.Name = res["secondplayer"]
+        if game_state["player_one"]["wins"] == 5:
+            print("one has won")
+            game_state["player_one"]["HasWon"] = True;
 
+        if game_state["player_two"]["wins"] == 5:
+            print("two has won")
+            game_state["player_two"]["HasWon"] = True;
 
-    def checkGameStatus():
-        if GamerOne.Points > 0 and GamerTwo.Points > 0:
-            return True
-        else:
-            return False
-
-    def checkIfWon():
-        if GamerOne.Wins < min_score_to_win and GamerTwo.Wins < min_score_to_win:
-            return True
-        else:
-            return False
-
-    while checkGameStatus() and checkIfWon():
-        input_player_one = int(input(f"{GamerOne.Name} Enter Number:"))
-        input_player_two = int(input(f"{GamerTwo.Name} Enter Number:"))
-
-        # if input is greater than the score left reset it to the score left
-        if input_player_one > GamerOne.Points:
-            input_player_one = GamerOne.Points
-
-        if input_player_two > GamerTwo.Points:
-            input_player_two = GamerTwo.Points
-
-        # repeat the game
-        # make function here
-        # reads file about me
-        while True:
-            GamerOne.Points -= input_player_one
-            GamerTwo.Points -= input_player_two
-
-            print(f"{GamerOne.Name} has left:{GamerOne.Points} points")
-            print(f"{GamerTwo.Name} has left: {GamerTwo.Points} points")
-
-            if input_player_one > input_player_two:
-                GamerOne.Wins += 1
-                print(f"player one won this round")
-
-            elif input_player_one < input_player_two:
-                GamerTwo.Wins += 1
-                print(f"player two won this round")
-            print(f"Score Player One: {GamerOne.Wins}")
-            print(f"Score Player One: {GamerTwo.Wins}")
-
-            break
-        print("-------------------")
-        checkGameStatus()
-
-    # if a player reached 0 points
-    # read save data
-    if GamerOne.Wins >= min_score_to_win or GamerTwo.Wins >= min_score_to_win:
-        if GamerOne.Wins > GamerTwo.Wins:
-            print(f"player one the GAME!")
-            playerOneHasWon = True
-        else:
-            print(f"player two the GAME!")
-            playerTwoHasWon = True
-    else:
-        print(f"Nobody won")
-
-
+    saveGameState(game_state)
